f5250220d571d5f72f74f4c9db80b6fbaf217059
Devi_Manoj <devimanojmanoj@gmail.com>
2025-07-14 20:28:39 +0500

    Add files via upload

---
+++ TernaryOpratorDemo.java
@@ -0,0 +1,19 @@
+packageModule1;
+import java.util.Scanner;
+
+public class TernaryOpratorDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the First Number: ");
+        int firstNumber = sc.nextInt();
+
+        System.out.print("Enter the Second Number: ");
+        int secondNumber = sc.nextInt();
+
+        int bigNumber = (firstNumber > secondNumber) ? firstNumber : secondNumber;
+
+        System.out.println("I am " + bigNumber + ", the Big Number");
+    }
+}
+
